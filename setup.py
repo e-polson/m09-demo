81adbcd84f2a247ef8d87f8852e3329812a68c28
e-polson <emmacpolson@gmail.com>
2025-03-18 20:13:05 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='http://github.com/e-polson/m09-demo',
+    author='R.C. Alvarado',
+    author_email='yfq7mg@virginia.edu',
+    license='MIT',
+    packages=['demo']
+)
